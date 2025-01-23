8059c825dc9b6e4eddf68c69059d635d48a8ff22
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2025-01-23 23:32:05 +0000

    ready for grading.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,17 @@
 //
-// Name
-// Date
+// Tatumn chavez
+// 1/23/25
 // Hello World Programming Project
 // COSC 1030
 //
 
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
