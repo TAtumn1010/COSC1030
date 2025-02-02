44be241d6a7d561b0346149a4d1ad55beb4b8bac
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2025-02-02 22:54:00 +0000

    ready to grade

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,20 +1,28 @@
 //
-// Name
-// Date
+// Tatumn chavez
+// 02/2/25
 // Restaurant Bill Programming Project
 // COSC 1030
-//
 
-// Variable declarations
+# include <iostream>
+using namespace std;
 
+int main(){
 // Constants for the tax rate and tip percentage
-
+    double meal = 88.67;
+    double tax  = meal * 0.0675;
 // Get the amount of the purchase.
-
+    double  subtotal = meal + tax;
 // Calculate the sales tax.
-
-// Calculate the tip.
-
+    double tip = subtotal * 0.20;
 // Calculate the total of the sale.
+    double total = subtotal + tip;
 
-// Print information about the sale.
+// display the charges.
+ cout << "meal cost: " << meal << endl;
+ cout << "tax: " << tax << endl;
+cout << "tip: " << tip << endl;
+cout << "total: " << total << endl;
+
+return 0;
+}
