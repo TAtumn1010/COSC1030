7cc07292637e13fe97308b0cd287eed5807affe8
TAtumn1010 <Tatumnchavez@westernwyoming.edu>
2025-02-03 01:12:06 +0000

    ready to grade

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,29 @@
-//
-// Name
-// Date
+
+// Tatumn chavez
+// 02/2/25
 // Ocean Levels Programming Project
 // COSC 1030
-//
+
+# include <iostream>
+using namespace std;
+
+int main(){
+// Constants for the ocean level rate per year. (millimeters)
+    double rate = 1.5;
+   
+// calculate the ocean level. 
+    double x = rate * 5;
+    double y = rate * 7;
+    double z = rate * 10;
+
+
+// display the amount the ocean level rose.
+ cout << " the ocean rose: " << x << " millimeters in 5 years."<<endl;
+ cout << " the ocean rose: " << y << " millimeters in 7 years."<<endl;
+ cout << " the ocean rose: " << z << " millimeters in 10 years."<<endl;
+ 
+return 0;
+}
+
 
 
